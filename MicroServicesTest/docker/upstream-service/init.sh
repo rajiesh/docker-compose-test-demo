b0d4eee60efa3e6e27e8613ce5334165681f8302
rajiesh <rajiesh.narayanan@gmail.com>
2020-01-17 10:35:36 +0530

    Add a gauge spec

--- MicroServicesTest/docker/upstream-service/init.sh
+++ MicroServicesTest/docker/upstream-service/init.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-yum install -y java-1.8.0-openjdk
+function use_jdk() {
+    if [ ! -f "${HOME}/.jabba/jabba.sh" ]; then
+      curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash
+    fi
+    source "${HOME}/.jabba/jabba.sh"
+    jabba install "$1=$2"
+    jabba use "$1"
+}
+
+  use_jdk "openjdk@1.12.0" "tgz+https://download.java.net/java/GA/jdk12/GPL/openjdk-12_linux-x64_bin.tar.gz"
+
+
+echo "JAVA_HOME set to : $JAVA_HOME"
+
 java -jar /upstream-service/upstream-service.jar
 
--- MicroServicesTest/src/test/java/StepImplementation.java
+++ MicroServicesTest/src/test/java/StepImplementation.java
@@ -1,47 +1,43 @@
 import com.thoughtworks.gauge.Step;
-import com.thoughtworks.gauge.Table;
-import com.thoughtworks.gauge.TableRow;
 
-import java.util.HashSet;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import org.junit.Assert;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class StepImplementation {
 
-    private HashSet<Character> vowels;
 
-    @Step("Vowels in English language are <vowelString>.")
-    public void setLanguageVowels(String vowelString) {
-        vowels = new HashSet<>();
-        for (char ch : vowelString.toCharArray()) {
-            vowels.add(ch);
-        }
-    }
+    @Step("Wait for the services to be up and running")
+    public void waitForServices() throws InterruptedException {
+        while(true) {
+            try {
+                System.out.println("This is before checking.....");
+                HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8080/api/get").openConnection();
+                connection.setRequestMethod("HEAD");
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    System.out.println("Service is up and running....");
+                    break;
+                }
+            } catch(Exception e) {
+                System.out.println("trying again...");
+                Thread.sleep(10000);// waiting 10 seconds before next try
 
-    @Step("The word <word> has <expectedCount> vowels.")
-    public void verifyVowelsCountInWord(String word, int expectedCount) {
-        int actualCount = countVowels(word);
-        Assert.assertEquals(expectedCount, actualCount);
-    }
-
-    @Step("Almost all words have vowels <wordsTable>")
-    public void verifyVowelsCountInMultipleWords(Table wordsTable) {
-        for (TableRow row : wordsTable.getTableRows()) {
-            String word = row.getCell("Word");
-            int expectedCount = Integer.parseInt(row.getCell("Vowel Count"));
-            int actualCount = countVowels(word);
-
-            Assert.assertEquals(expectedCount, actualCount);
-        }
-    }
-
-    private int countVowels(String word) {
-        int count = 0;
-        for (char ch : word.toCharArray()) {
-            if (vowels.contains(ch)) {
-                count++;
             }
         }
-        return count;
     }
+
+    @Step("Request to upstream service is successful")
+    public void verifyUpstreamAPI() throws InterruptedException {
+        when().
+                get("http://localhost:8080/api/get").
+                then().
+                statusCode(200).
+                body("modifiedKey1", equalTo("value1"),
+                        "modifiedKey2", equalTo("value2"));
+    }
+
+
 }
